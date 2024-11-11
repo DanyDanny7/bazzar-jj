03f12651488e7f98e3c74f880b99ce1eb965af71
DanyDanny7 <43179632+DanyDanny7@users.noreply.github.com>
2024-11-10 21:22:38 -0600

    fix id GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,6 @@ import GoogleTagManager from "@/lib/GoogleTagManager";
 export default ({ Component, pageProps }) => (
   <Fragment>
     <Component {...pageProps} />
-    <GoogleTagManager gtmId="GTM-KCZFX48J" />
+    <GoogleTagManager />
   </Fragment>
 )
