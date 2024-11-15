c51bbc48ce7ff89f1add80f5a30adaa993a51357
DanyDanny7 <43179632+DanyDanny7@users.noreply.github.com>
2024-11-15 01:22:07 -0600

    inicio de implementacion de agregar producto

--- src/components/admin/Form.jsx
+++ src/components/admin/Form.jsx
@@ -12,11 +12,6 @@ const defaultValues = {
 }
 
 const Form = ({ className, setOpen, onSubmit, toEdit, setToEdit, loading }) => {
-    console.log(loading)
-    console.log(loading)
-    console.log(loading)
-    console.log(loading)
-
     const [values, setValues] = useState(defaultValues);
 
     const isEdit = !isEmpty(toEdit)
