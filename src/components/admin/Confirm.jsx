8b94184af26f8e5347250b17f492be970e9e284d
DanyDanny7 <43179632+DanyDanny7@users.noreply.github.com>
2024-11-14 19:19:12 -0600

    añadir loading al componente de confirmación

--- src/components/admin/Confirm.jsx
+++ src/components/admin/Confirm.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
-export default function Example({ open, onConfirm, onCancel }) {
+export default function Example({ open, onConfirm, onCancel, loading }) {
 
     return (
         <Dialog open={open} onClose={onCancel} className="relative z-10">
@@ -47,6 +47,7 @@ export default function Example({ open, onConfirm, onCancel }) {
                                 data-autofocus
                                 onClick={onCancel}
                                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                loading={loading}
                             >
                                 Cancelar
                             </button>
