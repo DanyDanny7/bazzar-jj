ab5241a974ab9e5c970d07c1b5928abcefff9020
DanyDanny7 <43179632+DanyDanny7@users.noreply.github.com>
2024-11-16 23:08:00 -0600

    fix bug tabla productos

--- src/components/admin/Products.jsx
+++ src/components/admin/Products.jsx
@@ -5,7 +5,7 @@ import Button from "@/components/util/Button";
 
 const Products = ({ toEdit, onCancel, reload }) => (
     <div>
-        <TableProducts toEdit={toEdit} products={get(toEdit, "products", [])} reload={reload} />
+        <TableProducts toEdit={toEdit} products={get(toEdit, "products", [])} reload={reload} show />
         <div className="mt-6 flex items-end justify-start gap-x-6 w-full md:w-1/4 ml-auto">
             <Button type="button" onClick={onCancel}>
                 Cancelar
