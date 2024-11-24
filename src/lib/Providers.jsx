99e4904871108269a0310f5b50ae021f97472216
DanyDanny7 <43179632+DanyDanny7@users.noreply.github.com>
2024-11-24 01:28:37 -0600

    fix conflicts

--- src/lib/Providers.jsx
+++ src/lib/Providers.jsx
@@ -1,10 +1,14 @@
-'use client'
+'use client'// Indica que este componente se ejecuta en el lado del cliente
+// Importa React
 import React from 'react';
+// Importa el proveedor de sesión de Next Auth
 import { SessionProvider } from "next-auth/react";
 import ShoppingCartProvider from "@/components/layout/ShoppingCard/ContextShoppingCart"
 
+// Define el componente Session que recibe `children` como prop
 const Providers = ({ children }) => {
     return (
+        // Envuelve los hijos dentro del SessionProvider para proporcionar el contexto de la sesión
         <SessionProvider>
             <ShoppingCartProvider>
                 {children}
@@ -12,5 +16,5 @@ const Providers = ({ children }) => {
         </SessionProvider>
     )
 }
-
+// Exporta el componente Session como el valor por defecto
 export default Providers;
